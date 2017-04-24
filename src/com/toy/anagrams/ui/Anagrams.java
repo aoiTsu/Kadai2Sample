546e8b87b35c32595922da30562a9298df359ddf
aoiTsu <aoi@gm.tsuda.ac.jp>
2017-04-24 15:17:36 +0900

    UIのラベルを1つだけ日本語化

--- src/com/toy/anagrams/ui/Anagrams.java
+++ src/com/toy/anagrams/ui/Anagrams.java
@@ -150,7 +150,7 @@ public class Anagrams extends JFrame {
 
         guessLabel.setDisplayedMnemonic('Y');
         guessLabel.setLabelFor(guessedWord);
-        guessLabel.setText("Your Guess:");
+        guessLabel.setText("あなたの答え:");
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
